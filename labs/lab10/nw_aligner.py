9285fa1287a7d36456b823a46cc6b125807e311a
Paul Ngo <paulngo123@gmail.com>
2019-11-12 07:50:40 +0000

    finished lab10

--- labs/lab10/nw_aligner.py
+++ labs/lab10/nw_aligner.py
@@ -13,6 +13,8 @@ resulting alignment matrix and optimal alignment to STDOUT.
 
 import os
 import sys
+from Bio import SeqIO
+import numpy as np
 
 class NWAligner:
     def __init__(self, score_matrix_fname):
@@ -39,19 +41,35 @@ class NWAligner:
 
         score_matrix = {}
         gap_penalty = None
-
+        keys = []
+        non_comment_line_count = 0
         with open(fname) as fp:
             for line_num, line in enumerate(fp):
                 # ignore comments in matrix file
                 if line.startswith("#"):
                     continue
-
-                ### TODO ###
+                
+                ### TODO ### - DONE
                 # Parse matrix file line-by-line and load into nested dictionaries.
-                #
+                # 
                 # Last line of matrix contains the gap penalty which must be pulled
                 # out and returned.
-
+                if non_comment_line_count == 0: # we should create our keys
+                    keys = line.strip().split(" ")
+                elif non_comment_line_count == len(keys) + 1:
+                    gap_penalty = int(line.strip())
+                    score_matrix["-"] = {}
+                    for i in range(len(keys)):
+                        score_matrix["-"][keys[i]] = gap_penalty
+                        score_matrix[keys[i]]["-"] = gap_penalty
+                else:
+                    score_matrix[keys[non_comment_line_count - 1]] = {}
+                    line = line.strip().split(" ")
+                    for i in range(len(line)):
+                        score_matrix[keys[non_comment_line_count - 1]][keys[i]] = int(line[i])
+                
+                non_comment_line_count += 1
+                
         return score_matrix, gap_penalty
 
     @staticmethod
@@ -74,10 +92,15 @@ class NWAligner:
 
         seqs = []
 
-        ### TODO ###
+        ### TODO ### - DONE
         # Load FASTA file and return list of sequences.
         # Throw an error if there are more than two sequences in the file.
-
+        
+        records = list(SeqIO.parse(fname, "fasta"))
+        if len(records) != 2:
+            raise Exception('FASTA file does not contain exactly two sequences.')
+        seqs = [str(record.seq) for record in records]
+        
         return seqs
 
     def align(self, seq_x, seq_y, print_matrix = False):
@@ -104,12 +127,21 @@ class NWAligner:
         # create two empty matrices with sizes based on the input sequences.
         # one contains the dynamic programming matrix, the other contains
         # pointers we'll use during traceback
+        
         matrix = [[0] * (len(seq_y) + 1) for _ in range(len(seq_x) + 1)]
         pointers = [[0] * (len(seq_y) + 1) for _ in range(len(seq_x) + 1)]
 
-        ### TODO ###
+        ### TODO ### - DONE
         # Fill the top row of the matrix with scores for gaps.
         # Fill the first column of the matrix with scores for gaps.
+        
+        gap_penalty_increasing = 0
+        for i in range(0, max(len(seq_x), len(seq_y)) + 1):
+            if i <= len(seq_x):
+                matrix[i][0] = gap_penalty_increasing
+            if i <= len(seq_y):
+                matrix[0][i] = gap_penalty_increasing
+            gap_penalty_increasing += self.gap_penalty
 
         ###
         ### RECURSION
@@ -118,9 +150,9 @@ class NWAligner:
         # fill the dynamic programming and pointer matrices
         for x in range(1, len(seq_x) + 1):
             for y in range(1, len(seq_y) + 1):
-                match_score = self.score_matrix[seq_x[x - 1]][seq_y[y - 1]]
-
-                ### TODO ###
+                match_score = self.score_matrix[seq_x[x-1]][seq_y[y-1]]
+                
+                ### TODO ### - DONE
                 # Take the maximum score of three possibilities:
                 #   1) The element in the matrix diagonal from this one
                 #      plus the score of an exact match
@@ -131,11 +163,22 @@ class NWAligner:
                 # Keep track of which of these choices you made by setting
                 #   the same element (i.e., pointers[x][y]) to some value that
                 #   has meaning to you.
+                
+                values = [
+                    matrix[x-1][y-1] + match_score,
+                    matrix[x-1][y]   + self.gap_penalty,
+                    matrix[x][y-1]   + self.gap_penalty
+                ]
+                pointers[x][y] = np.argmax(values) + 1
+                matrix[x][y] = np.max(values)
 
         # print the dynamic programming matrix
         if print_matrix:
             for x in range(len(seq_x) + 1):
-                print " ".join(map(lambda i: str(int(i)), matrix[x]))
+                print("\t".join(map(lambda i: str(int(i)), matrix[x])))
+                
+            for x in range(len(seq_x) + 1):
+                print("\t".join(map(lambda i: str(int(i)), pointers[x]))) 
 
         ###
         ### TRACEBACK
@@ -156,9 +199,57 @@ class NWAligner:
             # Depending on which "move" you made at each element in the
             #   matrix, you'll either align seq_x to seq_y, seq_x to a gap, or
             #   seq_y to a gap.
-
+            
+            if move == 0:
+                # at the beginning
+                if x == 0:
+                    align_x.append("-")
+                    align_y.append(seq_y[y-1])
+                    y -= 1
+                else: # y == 0
+                    align_x.append(seq_x[x-1])
+                    align_y.append("-")
+                    x -= 1
+            elif move == 1:
+                # from the diagonal
+                align_x.append(seq_x[x-1])
+                align_y.append(seq_y[y-1])
+                x -= 1
+                y -= 1
+            elif move == 2:
+                # the character in sequence N was aligned to a gap, equivalent to move up
+                align_x.append(seq_x[x-1])
+                align_y.append("-")
+                x -= 1
+            elif move == 3:
+                # the character in sequence M was aligned to a gap, equivalent to move left
+                align_x.append("-")
+                align_y.append(seq_y[y-1])
+                y -= 1
+        self.matrix = matrix
+        self.pointers = pointers
         # flip the alignments, as they're reversed
-        return ("".join(align_x[::-1]), "".join(align_y[::-1]))
+        self.seq1 = "".join(align_x[::-1])
+        self.seq2 = "".join(align_y[::-1])
+        return (self.seq1, self.seq2)
+    
+    def score_seqs(self, seq1, seq2):
+        """
+        Input: (Strings) Two sequences that have been aligned (seq_x and seq_y).
+        Output: (Tuple of strings) The score, given the aligner's score_matrix.
+
+        Example:
+
+        >>> aligner = NWAligner('BLOSUM62')
+        >>> seqs = ('YAAD-SKATPGNPAF---HQDEIF--L-AR--IA-FIYQM-WDGGQLK-LIDYAPH-HVM-C---E-------E---', 'W---VGQ--P-N--MKVQH----WSNMKA-CCV-KFI---TW------TFI--APEKH--ACKWTETAYQADCDIIW')
+        >>> aligner.score_seqs(seqs[0], seqs[1])
+        16
+        """
+        score = 0
+        for i in range(len(seq1)):
+            score += self.score_matrix[seq1[i]][seq2[i]]
+        return score
+        
 
 ###                                      ###
 ### NO NEED TO EDIT CODE BELOW THIS LINE ###
@@ -166,7 +257,7 @@ class NWAligner:
 
 if __name__ == '__main__':
     def usage():
-        print 'usage: %s matrixfilename stringfilename'
+        print('usage: %s matrixfilename stringfilename')
         sys.exit(1)
 
     if len(sys.argv) != 3:
@@ -174,7 +265,7 @@ if __name__ == '__main__':
 
     for fname in sys.argv[1:]:
         if not os.path.isfile(fname):
-            print 'Can not open %s' % (fname,)
+            print('Can not open %s' % (fname,))
             usage()
 
     aligner = NWAligner(sys.argv[1])
